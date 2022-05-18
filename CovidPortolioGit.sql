5ff0faf77f1bb047dcb551f7425674df08331655
Kriti Gaur <86511722+KritiGaur@users.noreply.github.com>
2022-05-18 15:43:24 +0530

    Update CovidPortolioGit.sql

--- CovidPortolioGit.sql
+++ CovidPortolioGit.sql
@@ -1,6 +1,6 @@
 /*
 Covid 19 Data Exploration 
-Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+Skills used: Joins, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 */
 
 Select *
